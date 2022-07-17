40bc36cbf260e7f4a52b1bccb43e1ff4935b7970
voca021 <vfratric@gmail.com>
2022-07-17 19:34:04 +0200

    drugi build

--- app/src/main/com/vladafratric/vladafratric.java
+++ app/src/main/com/vladafratric/vladafratric.java
@@ -2,6 +2,6 @@ package com.vladafratric:
 
 public class vladafratric {
 	public static void main(String[] args) {
-		System.out.println("Moj prvi Grade build!");
+		System.out.println("Moj drugi Grade build!");
         }
 }
